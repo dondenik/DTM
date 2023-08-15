ba906964c03528e556a6f579c77e2e88af2edecb
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-08-16 11:21:19 +1200

    Optimised camera lerping so that it no longer branches

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const TURNING_SPEED = 0.1 #should be between 0-1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -21,10 +22,11 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
-	# Change Player Rotation to match Camera
-	if input_dir.y != 0 or input_dir.x != 0:
-		self.rotation.y = lerp(self.rotation.y, self.rotation.y + $CameraRoot.rotation.y, 0.1)
-		$CameraRoot.rotation.y -= $CameraRoot.rotation.y  * 0.1
+	# Change Player Rotation to match Camera if moving
+	# Note: if statement is somewhat inefficent
+	var is_moving = int((abs(input_dir.x) or abs(input_dir.y)))
+	self.rotation.y = lerp(self.rotation.y, self.rotation.y + $CameraRoot.rotation.y * is_moving, TURNING_SPEED)
+	$CameraRoot.rotation.y -= $CameraRoot.rotation.y  * TURNING_SPEED * is_moving
 	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
