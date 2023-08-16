73782f3532a3686a339417adc6b84b58219fae47
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-08-16 12:35:36 +1200

    changed turning speed

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const TURNING_SPEED = 0.1 #should be between 0-1
+const TURNING_SPEED = 0.2 #should be between 0-1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
