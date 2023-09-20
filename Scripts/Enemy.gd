2e3795efe90bd4441d9695a9064e95f9c1b65ae9
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-09-21 09:20:44 +1200

    Added prototype enemy ai

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,10 +4,15 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const TURNING_SPEED = 5.0
+const ATTACK_COOLDOWN = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var health = 100
+
+var attack_cooldown = 0
+var is_attacking = 0
+
 func _ready():
 	$Sprite3D.texture = $Sprite3D/SubViewport.get_texture()
 	$Sprite3D/SubViewport/Health.value = 100 - health
@@ -22,9 +27,6 @@ func _physics_process(delta):
 
 	var is_running = int((abs(input_dir.x) or abs(input_dir.y)))
 	
-	if is_running:
-		$AnimationPlayer.play("Run")
-	
 
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var player_position = get_parent().get_node("CharacterBody3D").position
@@ -35,12 +37,23 @@ func _physics_process(delta):
 	target_basis.z = 0
 	basis = basis.slerp(target_basis, TURNING_SPEED * delta).get_rotation_quaternion()
 	
+	attack_cooldown -= 1 * delta
+	
 	# check if near player:
 	var distance_to_player = self.global_position.distance_to(player_position)
+	if distance_to_player < 2:
+		if attack_cooldown <= 0:
+			$AnimationPlayer.play("Slash")
+			is_attacking = 1
+			attack_cooldown = ATTACK_COOLDOWN
+		else:
+			$AnimationPlayer.queue("Fight Idle")
+	elif is_running:
+		$AnimationPlayer.play("Run")
 	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * int(not (distance_to_player < 2))
+		velocity.z = direction.z * SPEED * int(not (distance_to_player < 2))
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
