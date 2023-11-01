b255f9a51d1baefb93bb3f49c902413da65a6846
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-11-01 13:14:19 +1300

    fixed issues with menu buttons

--- settingsmenu.gd
+++ settingsmenu.gd
@@ -4,4 +4,4 @@ extends VBoxContainer
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action_pressed("pause"):
-		get_tree().change_scene_to_packed(preload("res://main_menu.tscn"))
+		get_tree().change_scene_to_file("res://main_menu.tscn")
