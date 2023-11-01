e5d3b4d90a0b853dbd0f0189d1b74af0cfafe583
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-11-02 09:23:29 +1300

    Fixed dialogue

--- NPC.gd
+++ NPC.gd
@@ -9,6 +9,7 @@ var dialogue = ""
 @export var npc_dialogue_options = ["Gret"]
 var npc_dialogue_mode = "sequential"
 var npc_dialogue_counter = 0
+var player_in_range = false
 
 @onready var player = get_parent().get_node("CharacterBody3D")
 
@@ -49,18 +50,21 @@ func _physics_process(delta):
 
 func _on_talking_hitbox_body_entered(body):
 	body_in_talking_range.emit()
+	player_in_range = true
 	$Sprite3D.show()
 
 
 
 func _on_talking_hitbox_body_exited(body):
 	body_left_talking_range.emit()
+	player_in_range = false
 	$Sprite3D.hide()
 
 
 func _on_dialogue_request():
-	return_dialogue_request.emit(self.dialogue)
-	if npc_dialogue_mode == "sequential":
-		if self.npc_dialogue_counter < len(self.npc_dialogue_options) - 1:
-			self.npc_dialogue_counter += 1
-			self.dialogue = self.npc_dialogue_options[self.npc_dialogue_counter]
+	if player_in_range:
+		return_dialogue_request.emit(self.dialogue)
+		if npc_dialogue_mode == "sequential":
+			if self.npc_dialogue_counter < len(self.npc_dialogue_options) - 1:
+				self.npc_dialogue_counter += 1
+				self.dialogue = self.npc_dialogue_options[self.npc_dialogue_counter]
