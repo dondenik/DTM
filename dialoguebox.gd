dbbfa91dc3c64bfcc1d4a87bc6a7d11be7cef801
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-11-02 16:45:05 +1300

    added colour highlighting to important text

--- dialoguebox.gd
+++ dialoguebox.gd
@@ -4,14 +4,17 @@ extends Control
 func set_speaker(speaker):
 	$SpeakerName.text = speaker
 
+func _ready():
+	$Dialogue.set_use_bbcode(true)
 
 func display_text(text, id):
-	$Dialogue.text = ""
+	$Dialogue.text = text
+	$Dialogue.visible_characters = 0
 	self.show()
 	var current_id = id
 	for letter in text:
 		if get_parent().dia_counter == current_id:
-			$Dialogue.text = $Dialogue.text + letter
+			$Dialogue.visible_characters += 1
 			await get_tree().create_timer(0.02).timeout
 
 func hide_dialogue():
