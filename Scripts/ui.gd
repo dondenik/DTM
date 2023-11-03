fc912ba8d91dacf538dbdc5a203d1ea2887a7df0
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-11-03 22:58:00 +1300

    stuff ig

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -5,7 +5,7 @@ func _ready():
 	$VBoxContainer/RichTextLabel.add_theme_font_size_override("normal_font_size", int(round(23*Global.font_scale)))
 	$VBoxContainer/RichTextLabel.add_theme_font_size_override("normal_font_size", int(round(23*Global.font_scale)))
 	quest_update()
-
+	
 func bar_update(health, stamina):
 	$Health.value = 100 - health
 	$Stamina.value = 100 - stamina
