9b8261bf2c209e59514c7c3fd21277bdcbf3dc46
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-11-03 22:48:02 +1300

    added quest tracker

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,7 +1,16 @@
 extends Control
 
 
+func _ready():
+	$VBoxContainer/RichTextLabel.add_theme_font_size_override("normal_font_size", int(round(23*Global.font_scale)))
+	$VBoxContainer/RichTextLabel.add_theme_font_size_override("normal_font_size", int(round(23*Global.font_scale)))
+	quest_update()
+
 func bar_update(health, stamina):
 	$Health.value = 100 - health
 	$Stamina.value = 100 - stamina
+
+func quest_update():
+	$VBoxContainer/RichTextLabel.text = Global.main_quest
+	$VBoxContainer/RichTextLabel2.text = Global.sub_quest
 
