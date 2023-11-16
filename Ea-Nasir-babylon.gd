17ed1009a71719ed70873ca3e76f8382f473dc8f
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-11-16 13:08:35 +1300

    added story flags to babylon

--- Ea-Nasir-babylon.gd
+++ Ea-Nasir-babylon.gd
@@ -1,4 +1,8 @@
 extends "res://NPC.gd"
 
 func post_dialogue_func():
-	pass
+	Global.main_quest = "Wait for the copper to arrive"
+	Global.sub_quest = "Talk to Nanni"
+	Global.checkpointscene = "res://Ur 2.tscn"
+	$"../CharacterBody3D".ui.quest_update()
+	get_tree().change_scene_to_file("res://Ur 2.tscn")
