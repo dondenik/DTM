ed37b4df2f1038624a5f10e38150af462537b463
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-11-20 23:42:31 +1300

    stuff

---
+++ NanniUr4.gd
@@ -0,0 +1,7 @@
+extends "res://NanniUr3.gd"
+
+
+func _ready():
+	Global.main_quest = "Take the complaint tablet to Ea Nasir"
+	Global.sub_quest = "Find a way to the port"
+	$"../CharacterBody3D".ui.quest_update()
