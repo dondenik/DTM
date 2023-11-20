7299ec8d72edf5aeacbda592178e29962ed5c69b
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-11-20 17:05:03 +1300

    added ur 3 timer

---
+++ NanniUr3.gd
@@ -0,0 +1,54 @@
+extends "res://NanniUr2.gd"
+
+signal timer_start
+
+func _ready():
+	$AnimationPlayer.play("Thrust Slash")
+	$AnimationPlayer.pause()
+	$AnimationPlayer.seek(0.0)
+	self.body_in_talking_range.connect(player._in_dialogue_range)
+	self.body_left_talking_range.connect(player._left_dialogue_range)
+	player.request_dialogue.connect(_on_dialogue_request)
+	self.return_dialogue_request.connect(player._recieve_dialogue)
+	$Sprite3D.texture = $Sprite3D/SubViewport.get_texture()
+	self.dialogue = npc_dialogue_options[npc_dialogue_counter]
+	$AnimationPlayer.play("Thrust Slash")
+	$AnimationPlayer.pause()
+	$AnimationPlayer.seek(0.0)
+
+func _physics_process(delta):
+	$AnimationPlayer.play("Thrust Slash")
+	$AnimationPlayer.pause()
+	$AnimationPlayer.seek(0.5)
+	
+	### PHYSICS BASED MOVEMENT NOT REALLY USED ASIDE FROM GRAVITY
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	if move_along_path:
+		$AnimationPlayer.play("Walk")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var input_dir = Vector2(randf_range(-1,1),randf_range(-1,1))
+	var input_dir = Vector2(0, 0)
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if not move_along_path:
+		move_and_slide()
+	else:
+		get_parent().progress += SPEED * delta
+		if get_parent().progress_ratio >= 0.9:
+			post_destination_func()
+
+func post_dialogue_func():
+	Global.sub_quest = "30"
+	Global.main_quest = "Run to Ea Nasir before he leaves"
+	$"../CharacterBody3D".ui.quest_update()
+	timer_start.emit()
---
+++ Ur3Timer.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var timer_on = false
+var start_time = 30.0
+var time = 0.0
+
+func _on_nanni_timer_start():
+	self.timer_on = true
+	self.time = self.start_time
+
+func _process(delta):
+	if self.timer_on:
+		self.time -= delta
+		Global.sub_quest = "Time till Ea Nasir leaves: " + str(self.time).substr(0, 4)
+		$"../CharacterBody3D".ui.quest_update()
+		if self.time <= 0.0:
+			# kill the guy
+			print("bad")
