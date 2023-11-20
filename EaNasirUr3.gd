e75fd84dfd5af0820efd81e920ddf11f7affdfca
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-11-21 00:19:03 +1300

    fixed ur4 crashing

--- EaNasirUr3.gd
+++ EaNasirUr3.gd
@@ -8,3 +8,16 @@ func post_dialogue_func():
 	Global.main_quest = "Tell Nanni of Ea Nasir's refusal"
 	$"../CharacterBody3D".ui.quest_update()
 	self.talked_to = true
+
+func _on_dialogue_request():
+	if player_in_range and dialogue_enable:
+		get_node("../TimerControlNode").timer_on = false
+		return_dialogue_request.emit(self.dialogue, self.name)
+		if npc_dialogue_mode == "sequential":
+			if self.npc_dialogue_counter < len(self.npc_dialogue_options) - 1:
+				self.npc_dialogue_counter += 1
+				self.dialogue = self.npc_dialogue_options[self.npc_dialogue_counter]
+			else:
+				if no_repeating_enable:
+					self.dialogue_enable = false
+				post_dialogue_func()
