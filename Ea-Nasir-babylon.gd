5633f6a169b90aa155a6050ff2a52e7b1cc7fcdb
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-11-20 21:53:08 +1300

    quest update on scene run for babylon

--- Ea-Nasir-babylon.gd
+++ Ea-Nasir-babylon.gd
@@ -1,5 +1,10 @@
 extends "res://NPC.gd"
 
+func _ready():
+	Global.main_quest = "Deliver Ea-Nasir the money"
+	Global.sub_quest = "Find Ea-Nasir's house"
+	$"../CharacterBody3D".ui.quest_update()
+
 func post_dialogue_func():
 	Global.main_quest = "Wait for the copper to arrive"
 	Global.sub_quest = "Talk to Nanni"
--- Stairs.gd
+++ Stairs.gd
@@ -1,6 +1,9 @@
 extends Area3D
 
-
+func _ready():
+	Global.main_quest = "Deliver the complaint tablet to Ea-Nasir"
+	Global.sub_quest = "Head to Ea-Nasir's house"
+	$"../CharacterBody3D".ui.quest_update()
 
 func _on_body_entered(body):
 	Global.main_quest = "Deliver the complaint tablet to Ea-Nasir"
