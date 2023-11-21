568fc293db41e49ebd8b4b168c150d964accdb4b
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-11-21 17:44:26 +1300

    made it so that the continue screen has shader

--- ContinueScreen.gd
+++ ContinueScreen.gd
@@ -5,8 +5,8 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file(Global.checkpointscene)
+	%screenTrans.change_scene_to_file(Global.checkpointscene)
 
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	%screenTrans.change_scene_to_file("res://main_menu.tscn")
