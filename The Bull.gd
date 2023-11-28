d55d6291b010ff15818dca4ba308fc6df1d86bb5
Ben Adamson <ben.adamson@student.onslow.school.nz>
2023-11-28 23:07:09 +1300

    bull fixes

--- The Bull.gd
+++ The Bull.gd
@@ -4,6 +4,7 @@ func post_dialogue_func():
 	await timer(0.8)
 	self.hide()
 	var old_pos = self.position
+	old_pos.y = -0.35
 	get_parent().get_node("bull").position = old_pos
 	get_parent().get_node("bull").show()
 	get_parent().get_node("bull").AGGRO_DIST = 100
