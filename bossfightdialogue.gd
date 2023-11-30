062e493a8f87460e77d6bf6a342500d77242e9df
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-12-01 00:43:32 +1300

    made win con

--- bossfightdialogue.gd
+++ bossfightdialogue.gd
@@ -8,4 +8,6 @@ func post_dialogue_func():
 		self.get_node("../Ea Nasir Enemy").extension_func()
 	elif phase_counter == 3:
 		self.get_node("../pillarControl").start_pillarising()
+	elif phase_counter == 4:
+		%screenTrans.change_scene_to_file("res://main_menu.tscn")
 	self.dialogue_enable = false
