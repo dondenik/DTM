23a70f338d01cbbbe8c492dee26d90bc97bc1b96
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-12-02 22:15:39 +1300

    restored his ability ot return money

--- thiefEnemy.gd
+++ thiefEnemy.gd
@@ -3,3 +3,4 @@ extends "res://Scripts/Enemy.gd"
 func post_death_func():
 	Global.sub_quest = "Navigate to the port"
 	$"../CharacterBody3D".ui.quest_update()
+	self.get_node("../Raft Guy").has_money = true
