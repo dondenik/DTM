3003ab50a57ffd52ac2caced4e5f602c2f1213c8
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-12-02 22:03:09 +1300

    fixed the fix

--- main_menu_buttons.gd
+++ main_menu_buttons.gd
@@ -8,7 +8,7 @@ func _on_button_pressed():
 
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_packed(preload("res://Ur.tscn"))
+	%screenTrans.change_scene_to_file("Ur.tscn")
 
 
 func _on_button_3_pressed():
